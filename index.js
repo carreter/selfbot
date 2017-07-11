144d52768527f8cce636e8167b54270c7491dad4
palmerito0 <sandiegobutterflies@gmail.com>
2017-07-11 01:03:13 -0700

    Added chat logging function

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Import modules
 const Discord = require('discord.js');
+const fs = require('fs-extra');
+const path = require('path');
 
 // Read config file
 const config = require('./config.json');
@@ -18,12 +20,49 @@ client.on('message', (message) => {
   if (message.author !== client.user || !message.content.startsWith(config.prefix)) return;
 
   // Get command and arguments
-  const command = message.content.toLowerCase().slice(config.prefix.length);
+  const command = message.content.toLowerCase().slice(config.prefix.length).split(' ')[0];
   const args = message.content.toLowerCase().slice(config.prefix.length).split(' ').slice(1);
 
-  // Simple ping/pong command
-  if (command === 'ping') {
-    message.edit(`:information_source: Pong! | ${Date.now() - message.createdAt} ms`);
+  switch (command) {
+    // Simple ping/pong command
+    case 'ping': {
+      message.edit(`:information_source: Pong! | ${Date.now() - message.createdAt} ms`);
+      break;
+    }
+
+    // Save logs from current channel
+    // Args: # of messages, log filename
+    case 'savelogs': {
+      let logs;
+      const loglength = args[0];
+      const logfile = path.join('logs/', args[1]);
+      if (loglength === undefined) {
+        message.edit(':x: Error! Please provide number of messages to be saved.');
+        break;
+      }
+      if (logfile === undefined) {
+        message.edit(':x: Error! Please provide a file to save logs to.');
+        break;
+      }
+
+      message.edit(`:information_source: Attempting to save ${loglength} messages...`)
+        .then(() => message.channel.fetchMessages({ limit: loglength }))
+        .then((messages) => {
+          logs = messages;
+          message.edit(`:information_source: Found ${logs.size} messages, writing to file ${logfile}`);
+        })
+        .then(() => fs.outputFile(logfile, logs.array().map(msg => `${msg.createdAt.toTimeString().split(' ')[0]}: ${msg.author.username}: ${msg.content}`).reverse().join('\n')))
+        .then(() => message.edit(`:white_check_mark: Successfully saved ${logs.size} messages!`))
+        .catch((err) => {
+          message.edit(':x: Error! Please check bot logs.');
+          console.error(err);
+        });
+      break;
+    }
+    // Make a warning if command not recognized
+    default: {
+      console.warn(`${command} is not a valid command!`);
+    }
   }
 });
 
